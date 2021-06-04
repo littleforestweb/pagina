62081d7e5288b1676803d451895c04ec9c59aae1
xhico <francisco@littleforest.co.uk>
2021-06-04 18:51:04 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -19,7 +19,7 @@ async function getRequest(url) {
 async function addSidebar() {
     // set github repo URL
     var url = "https://raw.githubusercontent.com/littleforestweb/pagina/main/";
-    // var url = "https://bookmarklet.xhico:8443/";
+    // var url = "https://pagina.xhico:8443/";
 
     // Add Sidebar <html>
     const mySidebarHTML = await getRequest(url + "mySidebar.html");
