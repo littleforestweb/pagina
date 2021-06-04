00ee32b8e78213bd95de37eb56007dd9452614b3
xhico <31449326+xhico@users.noreply.github.com>
2021-06-04 18:13:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.body.style.marginLeft = "260px";
+  // document.body.style.marginLeft = "260px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.body.style.marginLeft = "0";
+  // document.body.style.marginLeft = "0";
 }
