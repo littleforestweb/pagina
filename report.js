676c3393ae99647682f47ac43b74c00af134d8d4
xhico <francisco@littleforest.co.uk>
2021-06-04 20:17:49 +0100

    .

--- report.js
+++ report.js
@@ -1,29 +1,9 @@
-$(document).ready(function () {
-    var element = document.getElementById('resizable');
-    if (element) {
-        var resizer = document.createElement('div');
-        resizer.className = 'draghandle';
-        resizer.style.width = '6px';
-        resizer.style.height = '100vh';
-        element.appendChild(resizer);
-        resizer.addEventListener('mousedown', initResize, false);
-    }
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.body.style.marginLeft = "250px";
+}
 
-    function initResize(e) {
-        window.addEventListener('mousemove', Resize, false);
-        window.addEventListener('mouseup', stopResize, false);
-        $('#mainArea.content, #tabs iframe').addClass('marginLeft');
-    }
-
-    function Resize(e) {
-        element.style.width = (e.clientX - element.offsetLeft) + 'px';
-        $('#mainArea.content').css('margin-left', (e.clientX - element.offsetLeft) + 'px');
-        $('#tabs iframe').css('margin-left', (element.offsetLeft + 40) + 'px');
-    }
-
-    function stopResize(e) {
-        window.removeEventListener('mousemove', Resize, false);
-        window.removeEventListener('mouseup', stopResize, false);
-        $('#tabs iframe').css('margin-left', element.offsetLeft + 'px');
-    }
-});
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.body.style.marginLeft = "0";
+}
