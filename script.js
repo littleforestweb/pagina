2556aaa3c3913398ed614f4fdc2991560104e486
xhico <francisco@littleforest.co.uk>
2021-06-04 19:05:38 +0100

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.body.style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  // document.body.style.marginLeft = "0";
-}
