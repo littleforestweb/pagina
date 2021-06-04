d8fcb1c9bb6f8013378da3a0e9e49a15069f3914
xhico <francisco@littleforest.co.uk>
2021-06-04 21:04:03 +0100

    .

--- report.js
+++ report.js
@@ -3,11 +3,11 @@ var mini = true;
 function toggleSidebar() {
     if (mini) {
         document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("LFiwrap").style.marginLeft = "250px";
+        document.getElementById("mywrap").style.marginLeft = "250px";
         this.mini = false;
     } else {
         document.getElementById("mySidebar").style.width = "85px";
-        document.getElementById("LFiwrap").style.marginLeft = "85px";
+        document.getElementById("mywrap").style.marginLeft = "85px";
         this.mini = true;
     }
 }
