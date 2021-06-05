61af85b58d2f7cd8d2becf2e461d2095c225cd5a
xhico <francisco@littleforest.co.uk>
2021-06-05 16:11:06 +0100

    .

--- sidebar.js
+++ sidebar.js
@@ -9,11 +9,11 @@ function toggleSidebar() {
         document.getElementById("lfi_img").style.marginLeft = "auto";
         this.mini = false;
     } else {
-        // document.getElementById("mySidebar").style.width = "85px";
-        // document.getElementById("mywrap").style.marginLeft = "85px";
-        // document.getElementById("lfi_img").src = "https://raw.githubusercontent.com/littleforestweb/pagina/main/forest.png"
-        // document.getElementById("lfi_img").style.marginRight = "auto";
-        // document.getElementById("lfi_img").style.marginLeft = "unset";
-        // this.mini = true;
+        document.getElementById("mySidebar").style.width = "85px";
+        document.getElementById("mywrap").style.marginLeft = "85px";
+        document.getElementById("lfi_img").src = "https://raw.githubusercontent.com/littleforestweb/pagina/main/forest.png"
+        document.getElementById("lfi_img").style.marginRight = "auto";
+        document.getElementById("lfi_img").style.marginLeft = "unset";
+        this.mini = true;
     }
 }
