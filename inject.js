484e3bb9f56c1db19022f2b60136096b93b9ad2b
xhico <francisco@littleforest.co.uk>
2021-06-06 22:35:30 +0100

    Auto Open Sidebar

--- inject.js
+++ inject.js
@@ -27,8 +27,8 @@ async function addSidebar() {
     document.body.appendChild(div);
 
     // set github repo URL
-    var url = "https://raw.githubusercontent.com/littleforestweb/pagina/main/";
-    // var url = "https://pagina.xhico:8443/";
+    // var url = "https://raw.githubusercontent.com/littleforestweb/pagina/main/";
+    var url = "https://pagina.xhico:8443/";
 
     // Add LFisidebar <html>
     var reportHTML = await getRequest(url + "report.html");
@@ -123,6 +123,9 @@ async function main() {
     } else {
         console.log("Already checked.. nothing to do!");
     }
+
+    // Open Sidebar
+    document.getElementById("openSidebar").click();
 
     // END
     console.log('inject ended');
