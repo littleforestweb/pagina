baa56c949928f68ed4ce4f5556975b23db7e0df3
xhico <francisco@littleforest.co.uk>
2021-06-07 23:17:38 +0100

    dropdown

--- report.js
+++ report.js
@@ -1,23 +1,37 @@
-const netflix_open_btn = document.querySelector('.floating-btn');
-const netflix_close_btn = document.querySelector('.netflix-close-btn');
-const netflix_nav = document.querySelectorAll('.netflix-nav');
-const maincontent = document.getElementById('maincontent');
+// const netflix_open_btn = document.querySelector('.floating-btn');
+// const netflix_close_btn = document.querySelector('.netflix-close-btn');
+// const netflix_nav = document.querySelectorAll('.netflix-nav');
+// const maincontent = document.getElementById('maincontent');
+
+// netflix_open_btn.addEventListener('click', () => {
+//     netflix_nav.forEach(nav_el => { nav_el.classList.add('visible'); });
+//     maincontent.style.marginLeft = "300px";
+//     maincontent.classList.remove("iframe-width-100");
+//     maincontent.classList.add("iframe-width-300");
+// });
+
+// netflix_close_btn.addEventListener('click', () => {
+//     netflix_nav.forEach(nav_el => { nav_el.classList.remove('visible'); });
+//     maincontent.style.marginLeft = "0px";
+//     maincontent.classList.remove("iframe-width-300");
+//     maincontent.classList.add("iframe-width-100");
+// });
+
+
 const spellErrors = document.getElementById('spell-ul');
-
-netflix_open_btn.addEventListener('click', () => {
-    netflix_nav.forEach(nav_el => { nav_el.classList.add('visible'); });
-    maincontent.style.marginLeft = "300px";
-    maincontent.classList.remove("iframe-width-100");
-    maincontent.classList.add("iframe-width-300");
-});
-
-netflix_close_btn.addEventListener('click', () => {
-    netflix_nav.forEach(nav_el => { nav_el.classList.remove('visible'); });
-    maincontent.style.marginLeft = "0px";
-    maincontent.classList.remove("iframe-width-300");
-    maincontent.classList.add("iframe-width-100");
-});
-
 spellErrors.addEventListener('click', () => {
-    document.getElementById("spell-li").style.display = "None";
+    if (document.getElementById("spell-li").style.display == "none") {
+        document.getElementById("spell-li").style.display = "block";
+    } else {
+        document.getElementById("spell-li").style.display = "none";
+    }
+});
+
+const generalInfo = document.getElementById('general-ul');
+generalInfo.addEventListener('click', () => {
+    if (document.getElementById("general-li").style.display == "none") {
+        document.getElementById("general-li").style.display = "block";
+    } else {
+        document.getElementById("general-li").style.display = "none";
+    }
 });
