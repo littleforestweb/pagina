a9ef37f60fe31418449ec3cbde8c8506951d29de
xhico <francisco@littleforest.co.uk>
2021-06-07 16:20:42 +0100

    .

--- inject.js
+++ inject.js
@@ -31,7 +31,6 @@ async function addSidebar() {
 
     // set github repo URL
     var url = "https://raw.githubusercontent.com/littleforestweb/pagina/main/";
-    // var url = "https://pagina.xhico:8443/";
 
     // Add LFisidebar <html>
     var reportHTML = await getRequest(url + "report.html");
@@ -48,67 +47,68 @@ async function addSidebar() {
     document.head.appendChild(report).innerHTML = depCSS;
 }
 
-async function runLangTool(tagName, lang) {
+async function runLangTool(lang) {
     const iframe = document.getElementById('maincontent');
-    iframe.addEventListener("load", async function () {
-        const iframeContent = iframe.contentDocument;
+    const iframeContent = iframe.contentDocument;
 
-        // get all tags
-        const tags = iframeContent.getElementsByTagName(tagName);
+    // get all tags
+    const tags = iframeContent.getElementsByTagName("p");
 
-        // set errorsDict where key => error and value => [count, color]
-        var eDict = {};
+    // set errorsDict where key => error and value => [count, color]
+    var eDict = {};
 
-        // iterate on every tag
-        for (var i = 0; i < tags.length; i++) {
+    // iterate on every tag
+    for (var i = 0; i < tags.length; i++) {
 
-            // set phrase from content array index
-            var tag = tags[i]
+        // set phrase from content array index
+        var tag = tags[i]
 
-            //  LanguageTool URL
-            const url = "https://api.languagetoolplus.com/v2/check?text=" + tag.innerHTML.replace(/<\/?[^>]+(>|$)/g, "") + "&language=" + lang;
+        //  LanguageTool URL
+        const url = "https://api.languagetoolplus.com/v2/check?text=" + tag.innerHTML.replace(/<\/?[^>]+(>|$)/g, "") + "&language=" + lang;
 
-            // get LangTool API Response
-            const data = await getRequest(url);
+        // get LangTool API Response
+        const data = await getRequest(url);
 
-            try {
+        try {
 
-                // iterate on every error
-                data.matches.forEach(function (entry) {
+            // iterate on every error
+            data.matches.forEach(function (entry) {
 
-                    // get error, message;
-                    var text = entry.context.text; var message = entry.message; let color;
-                    var error = text.substring(entry.context.offset, entry.context.offset + entry.context.length);
+                // get error, message;
+                var text = entry.context.text; var message = entry.message; let color;
+                var error = text.substring(entry.context.offset, entry.context.offset + entry.context.length);
 
-                    // remove false-positive errors (one char and whitespaces)
-                    if (error.length >= 3 && !(/\s/g.test(error))) {
+                // remove false-positive errors (one char and whitespaces)
+                if (error.length >= 3 && !(/\s/g.test(error))) {
 
-                        // set color of error => red for mistake and yellow for others
-                        if (message == "Possible spelling mistake found.") { color = "red"; } else { color = "orange"; }
+                    // set color of error => red for mistake and yellow for others
+                    if (message == "Possible spelling mistake found.") { color = "red"; } else { color = "orange"; }
 
-                        // update error color on html
-                        tag.innerHTML = tag.innerHTML.replace(error,
-                            "<span id='lfi_" + error + "' " + "title='" + message + "' style='color:" + color + ";font-weight:bold;'>" + error + "</span>"
-                        );;
+                    // update error color on html
+                    tag.innerHTML = tag.innerHTML.replace(error,
+                        "<span id='lfi_" + error + "' " + "title='" + message + "' style='color:" + color + ";font-weight:bold;'>" + error + "</span>"
+                    );;
 
-                        // add/update key error on eDict
-                        if (error in eDict) { eDict[error][0] = eDict[error][0] + 1; } else { eDict[error] = [1, color]; }
+                    // add/update key error on eDict
+                    if (error in eDict) {
+                        eDict[error][0] = eDict[error][0] + 1;
+                    } else {
+                        eDict[error] = [1, color, message];
                     }
-                });
+                }
+            });
 
-            } catch (error) {
-                continue;
-            }
+        } catch (error) {
+            continue;
         }
+    }
 
-        // Add errors to LFisidebar
-        var sidebar = document.getElementById("spellErrors")
-        Object.entries(eDict).forEach(([key, value]) => {
-            var error = key; var count = value[0]; var color = value[1];
-            console.log(key, count, color);
-            sidebar.innerHTML += "<li><a href='#lfi_" + error + "'>" + error + " (" + count + ")" + "</a></li>";
-        });
-
+    // Add errors to LFisidebar
+    var sidebar = document.getElementById("spellErrors")
+    Object.entries(eDict).forEach(([key, value]) => {
+        var error = key; var count = value[0]; var color = value[1]; var message = value[2];
+        console.log(key, count, color);
+        sidebar.innerHTML += "<li><a title='" + message + "' href='#lfi_" + error + "'>" + error + " (" + count + ")" + "</a></li>";
     });
 
 }
@@ -117,8 +117,13 @@ async function main() {
     // Add sidebar
     await addSidebar();
 
-    // // Run languageTool on tagName using lang
-    await runLangTool("p", "en-GB");
+    // Run languageTool once iframe has loaded
+    document.getElementById('maincontent').addEventListener("load", async function () {
+        // // Run languageTool on tagName using lang
+        await runLangTool("en-GB");
+
+    });
+
 }
 
 (async function () {
