110a2aa7a3be64fdc15ac1f39e20f92d0882fba4
xhico <31449326+xhico@users.noreply.github.com>
2021-07-14 09:44:06 +0100

    .

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('checkPage').addEventListener('click', function () {
         let selectedLanguage = document.getElementById("languagesList").value;
-
         chrome.runtime.sendMessage({ language: selectedLanguage }, function (response) {
             this.close();
         });
