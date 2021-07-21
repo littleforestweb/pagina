98242824bdca1ed7dfc3ea304d8963fe5cd0dc43
xhico <31449326+xhico@users.noreply.github.com>
2021-07-21 18:39:11 +0100

    Settings menu

--- assets/report.js
+++ assets/report.js
@@ -1,4 +1,13 @@
-const lfi_spelling_errors = document.getElementById('lfi_spelling-title');
+let lfi_settings = document.getElementById('lfi_settings-title');
+lfi_settings.addEventListener('click', () => {
+    if (document.getElementById("lfi_settings-li").style.display == "none") {
+        document.getElementById("lfi_settings-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_settings-li").style.display = "none";
+    }
+});
+
+let lfi_spelling_errors = document.getElementById('lfi_spelling-title');
 lfi_spelling_errors.addEventListener('click', () => {
     if (document.getElementById("lfi_spelling-li").style.display == "none") {
         document.getElementById("lfi_spelling-li").style.display = "block";
@@ -7,7 +16,7 @@ lfi_spelling_errors.addEventListener('click', () => {
     }
 });
 
-const lighthouse = document.getElementById('lfi_lighthouse-title');
+let lighthouse = document.getElementById('lfi_lighthouse-title');
 lighthouse.addEventListener('click', () => {
     if (document.getElementById("lfi_lighthouse-li").style.display == "none") {
         document.getElementById("lfi_lighthouse-li").style.display = "block";
@@ -16,7 +25,7 @@ lighthouse.addEventListener('click', () => {
     }
 });
 
-const generalInfo = document.getElementById('lfi_general-title');
+let generalInfo = document.getElementById('lfi_general-title');
 generalInfo.addEventListener('click', () => {
     if (document.getElementById("lfi_general-li").style.display == "none") {
         document.getElementById("lfi_general-li").style.display = "block";
