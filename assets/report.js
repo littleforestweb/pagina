39009bcf5845b8156589e07a358f7af3de9674eb
xhico <31449326+xhico@users.noreply.github.com>
2021-07-22 12:50:44 +0100

    .

--- assets/report.js
+++ assets/report.js
@@ -1,22 +1,78 @@
 // ------------------ Toggle Sidebar Section On/Off ------------------ //
 
 // Default hide every section apart from Settings and General
-// document.getElementById("lfi_settings-li").style.display = "none"
-// document.getElementById("lfi_accessibility-li").style.display = "none"
-// document.getElementById("lfi_content-li").style.display = "none"
-// document.getElementById("lfi_links-li").style.display = "none"
-// document.getElementById("lfi_spelling-li").style.display = "none"
-// document.getElementById("lfi_lighthouse-li").style.display = "none"
-// document.getElementById("lfi_technologies-li").style.display = "none"
+document.getElementById("lfi_settings-li").style.display = "none"
+document.getElementById("lfi_accessibility-li").style.display = "none"
+document.getElementById("lfi_content-li").style.display = "none"
+document.getElementById("lfi_links-li").style.display = "none"
+document.getElementById("lfi_spelling-li").style.display = "none"
+document.getElementById("lfi_lighthouse-li").style.display = "none"
+document.getElementById("lfi_technologies-li").style.display = "none"
+
 
 // SETTINGS
-let lfi_settings = document.getElementById('lfi_settings-title');
-lfi_settings.addEventListener('click', () => {
+document.getElementById('lfi_settings-title').addEventListener('click', () => {
     if (document.getElementById("lfi_settings-li").style.display == "none") {
         document.getElementById("lfi_settings-li").style.display = "block";
     } else {
         document.getElementById("lfi_settings-li").style.display = "none";
     }
 });
+
+// ACCESSIBILITY
+document.getElementById('lfi_accessibility-title').addEventListener('click', () => {
+    if (document.getElementById("lfi_accessibility-li").style.display == "none") {
+        document.getElementById("lfi_accessibility-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_accessibility-li").style.display = "none";
+    }
+});
+
+// CONTENT
+document.getElementById('lfi_content-title').addEventListener('click', () => {
+    if (document.getElementById("lfi_content-li").style.display == "none") {
+        document.getElementById("lfi_content-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_content-li").style.display = "none";
+    }
+});
+
+// LINKS
+document.getElementById('lfi_links-title').addEventListener('click', () => {
+    if (document.getElementById("lfi_links-li").style.display == "none") {
+        document.getElementById("lfi_links-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_links-li").style.display = "none";
+    }
+});
+
+// SPELLING
+document.getElementById('lfi_spelling-title').addEventListener('click', () => {
+    if (document.getElementById("lfi_spelling-li").style.display == "none") {
+        document.getElementById("lfi_spelling-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_spelling-li").style.display = "none";
+    }
+});
+
+// LIGHTHOUSE
+document.getElementById('lfi_lighthouse-title').addEventListener('click', () => {
+    if (document.getElementById("lfi_lighthouse-li").style.display == "none") {
+        document.getElementById("lfi_lighthouse-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_lighthouse-li").style.display = "none";
+    }
+});
+
+
+// TECHNOLOGIES
+document.getElementById('lfi_technologies-title').addEventListener('click', () => {
+    if (document.getElementById("lfi_technologies-li").style.display == "none") {
+        document.getElementById("lfi_technologies-li").style.display = "block";
+    } else {
+        document.getElementById("lfi_technologies-li").style.display = "none";
+    }
+});
+
 
 // -------------------------------------------------------------------------------------- //
