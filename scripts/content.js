4fd47783db138c8b82ee8de32ec489ec4ea1aa8d
xhico <31449326+xhico@users.noreply.github.com>
2021-07-22 13:15:26 +0100

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,16 @@
 console.clear();
 
-// Wait for Lighthouse Report Button to exist
+// -------------------------------------------------------------------------------------- //
+
 setInterval(function () {
+
     // Check if Lighthouse Report Button is clicked
     if (document.getElementById('runLighthouse')) {
         document.getElementById('runLighthouse').onclick = function () {
             runLighthouse();
         };
     }
-}, 100);
 
-// Wait for Lighthouse Report Button to exist
-setInterval(function () {
     // Check if Lighthouse Report Button is clicked
     if (document.getElementById('lfi_languages_list')) {
         document.getElementById('lfi_languages_list').onchange = async function () {
@@ -19,4 +18,5 @@ setInterval(function () {
             await runLanguageTool();
         };
     }
+
 }, 100);
